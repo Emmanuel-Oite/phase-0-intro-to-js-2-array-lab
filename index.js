e61d28b7155ed0f5ba751eef383b2bd42e56d230
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2023-10-25 09:50:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+//destructivelyAppendCat(name):
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+//destructivelyPrependCat(name):
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+//destructivelyRemoveLastCat():
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+//destructivelyRemoveFirstCat():
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+//appendCat(name):
+function appendCat(name) {
+  let newCats = [...cats, name];
+  return newCats;
+}
+//prependCat(name)
+function prependCat(name) {
+  let newCats = [name, ...cats];
+  return newCats;
+}
+
+//removeLastCat()
+function removeLastCat() {
+  let newCats = cats.slice(0, -1);
+  return newCats;
+}
+
+//removeFirstCat()
+function removeFirstCat() {
+  let newCats = cats.slice(1);
+  return newCats;
+}
